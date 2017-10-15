6a1f270ff1b5c7919677cfd16fb6f175c18aa348
Venson Joy Almeida <31315950+vensonjoyalmeida@users.noreply.github.com>
2017-10-15 12:46:14 -0500

    Create createdb.php

---
+++ createdb.php
@@ -0,0 +1,25 @@
+<?php
+include('dbdetails.php');
+try {
+    $conn = new PDO("mysql:host=$endpoint;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // sql to create table
+    $sql = "CREATE TABLE form (
+    email VARCHAR(40) NOT NULL,
+    password VARCHAR(40),
+    image VARCHAR(40)
+    )";
+
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Table MyGuests created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
